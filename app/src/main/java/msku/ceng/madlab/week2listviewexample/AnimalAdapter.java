273983c55b85ce77c68983beb05eb2caa21b086d
ozbayirmak@posta.mu.edu.tr <ozbayirmak@posta.mu.edu.tr>
2025-10-01 22:18:46 +0300

    week2

---
+++ app/src/main/java/msku/ceng/madlab/week2listviewexample/AnimalAdapter.java
@@ -0,0 +1,51 @@
+package msku.ceng.madlab.week2listviewexample;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AnimalAdapter extends BaseAdapter {
+
+    private LayoutInflater mInflater;
+    private List<Animal> animals;
+
+    public AnimalAdapter(Activity activity, List<Animal> animals) {
+        mInflater = (LayoutInflater) activity.getSystemService( Context.LAYOUT_INFLATER_SERVICE);
+        this.animals = animals;
+    }
+
+    public int getCount() {
+        return animals.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return animals.get(i);
+    }
+
+    public long getItemId(int i) {
+        return i;
+    }
+
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View rowView;
+
+        rowView = mInflater.inflate(R.layout.listview_row, null);
+        TextView textView = (TextView) rowView.findViewById(R.id.label);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.pic);
+
+        Animal animal = animals.get(i);
+
+        textView.setText(animal.getType());
+        imageView.setImageResource(animal.getPicId());
+
+        return rowView;
+    }
+}
